82aad162609601312dccb37bc915862aae8e992c
Łukasz Kaczor <lukaszk9396@gmail.com>
2019-11-02 23:14:16 +0100

    Added some elements to index.html

--- script.js
+++ script.js
@@ -5,8 +5,47 @@ $(".hamburgerMenu").on("click", function () {
 });
 
 $(".fa-search").on("click", function () {
- //   $(".searchForm").toggleClass("displayFlex");
+    //   $(".searchForm").toggleClass("displayFlex");
     $(".searchForm").toggleClass("showSearchForm");
-   //$(".test").fadeToggle()
+
+
+    //$(".test").fadeToggle()
 
 });
+
+
+
+//Oskryptowanie okazji dnia
+var sold = $(".soldValue").text();
+var left = $(".leftValue").text();
+var total = parseInt(sold) + parseInt(left);
+
+var percent = (sold / total) * 100
+
+setProgressBarWidth();
+
+function setProgressBarWidth() {
+    $(".progress").css("width", percent + "%");
+
+    if (left == 0) {
+        $(".timeLeft").text("");
+        $(".timeCaption").text("Wyprzedano!");
+    }
+}
+
+setInterval(() => {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    var hoursLeft = 24 - hours;
+    var minutesLeft = 59 - minutes;
+    var secondsLeft = 59 - seconds
+
+    if (hoursLeft < 10) hoursLeft = "0" + hoursLeft
+    if (minutesLeft < 10) minutesLeft = "0" + minutesLeft
+    if (secondsLeft < 10) secondsLeft = "0" + secondsLeft
+
+    $(".timeLeft").text(hoursLeft + ":" + minutesLeft + ":" + secondsLeft);
+}, 1000);
