9f92bc3731b21ce017b770cbe8fb65e23b32d2b6
Łukasz Kaczor <lukaszk9396@gmail.com>
2019-11-13 19:29:16 +0100

    Improved some animations

--- JS/brands.js
+++ JS/brands.js
@@ -1,185 +1,75 @@
 const quantity = $(".brand").length;
-var elementsInOneBox = 5; //5
-var brandBoxWidth = 230; //230
-var pos = 0;
+var elementsInOneBox = getElementsInTheBox(); //5
+var brandBoxWidth = getBrandBoxWidth(); //230
+var position = 0;
 
+var elementsPerOneLine = parseInt(quantity / elementsInOneBox);
+var constElementsPerOneLine = elementsPerOneLine;
+var rest = quantity % elementsInOneBox;
 
-var cale = parseInt(quantity / elementsInOneBox);
-const a = cale;
-var reszta = quantity % elementsInOneBox;
-var flagNext = false;
+var contentWidth = document.querySelector(".content").offsetWidth; //SZEROKOSC CONTENTU
+var restWidth = contentWidth * (rest / 100); //szerokosc "reszty"
+var brandsLineWidth = parseInt((contentWidth * (elementsPerOneLine - 1) + restWidth)); //szerokosc jednego "paska (z elementsInTheBox sztuk*szerokosc contentu )  //plus szerokosc reszty"
 
-var cw = document.querySelector(".content").offsetWidth; //SZEROKOSC CONTENTU
-var dudu = cw * (reszta/100);  //szerokosc "reszty"
-var rr = parseInt((cw*(cale-1)+dudu)); //szerokosc jednego "paska (z elementsInTheBox sztuk*szerokosc contentu )  //plus szerokosc reszty"
 
 $(".next").on('click', function () {
-
-    if (pos < rr) {
-
-        if (cale - 1 > 0) {
-            pos += elementsInOneBox * brandBoxWidth;
-            cale--;
+    if (position < brandsLineWidth) {
+        if (elementsPerOneLine - 1 > 0) {
+            position += elementsInOneBox * brandBoxWidth;
+            elementsPerOneLine--;
         } else {
-            pos += reszta * brandBoxWidth;
+            position += rest * brandBoxWidth;
         }
         translateBrands();
     }
 });
 
 $(".prev").on('click', function () {
-    if (pos > 1) {
-        if (reszta == 0 || cale < a) {
-            pos -= elementsInOneBox * brandBoxWidth;
-            cale++;
+    if (position > 1) {
+        if (rest == 0 || elementsPerOneLine < constElementsPerOneLine) {
+            position -= elementsInOneBox * brandBoxWidth;
+            elementsPerOneLine++;
         } else {
-            pos -= reszta * brandBoxWidth;
+            position -= rest * brandBoxWidth;
         }
         translateBrands();
     }
 });
 
-
-//DOBRE
-/*
-const quantity = $(".brand").length;
-var elementsInOneBox = 3; //5
-var brandBoxWidth = 230; //230
-var pos = 0;
-
-
-
-
-var cale = parseInt(quantity / elementsInOneBox);
-const a = cale;
-var reszta = quantity % elementsInOneBox;
-var flagNext = false;
-
-
-
-var cw = document.querySelector(".content").offsetWidth; //SZEROKOSC CONTENTU
-
-var dudu = cw * (reszta/100);  //szerokosc "reszty"
-
-
-var rr = parseInt((cw*(cale-1)+dudu)); //szerokosc jednego "paska (z elementsInTheBox sztuk*szerokosc contentu )  //plus szerokosc reszty"
-alert(rr);
-
-
-
-
-
-
-$(".brandsCaption").text(pos);
-$(".next").on('click', function () {
-
-    if (pos < rr) {
-
-        if (cale - 1 > 0) {
-            pos += elementsInOneBox * brandBoxWidth;
-            cale--;
-        } else {
-            pos += reszta * brandBoxWidth;
-        }
-
-
-        translateBrands();
-    }
-    $(".brandsCaption").text(pos);
-});
-
-
-$(".prev").on('click', function () {
-
-    if (pos > 1) {
-      
-
-        if (reszta == 0 || cale < a) {
-            pos -= elementsInOneBox * brandBoxWidth;
-            cale++;
-        } else {
-            pos -= reszta * brandBoxWidth;
-        }
-
-        translateBrands();
-        $(".brandsCaption").text(pos);
-    }
-});
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const quantity = $(".brand").length;
-var elementsInOneBox = 5; //5
-var brandBoxWidth = 230; //230
-var left = quantity - elementsInOneBox;
-var tutej = 0;
-var flag = false;
-
-$(".next").on('click', function () {
-    flag = true;
-    if(left>=elementsInOneBox)
-    {
-        left-=elementsInOneBox;
-        tutej += brandBoxWidth*elementsInOneBox;
-    }
-    else{
-        tutej += brandBoxWidth*(left%elementsInOneBox);
-        left = 0;
-    }
-
+//dostosowanie wszystkich ustawien do szerokosci contentu i zresetowanie pozycji do 0 w celu unikniecia bugów 
+$(window).resize(function () {
+    elementsInOneBox = getElementsInTheBox();
+    brandBoxWidth = getBrandBoxWidth();
+    elementsPerOneLine = parseInt(quantity / elementsInOneBox);
+    position = 0;
+    contentWidth = document.querySelector(".content").offsetWidth;
+    restWidth = contentWidth * (rest / 100);
+    brandsLineWidth = parseInt((contentWidth * (elementsPerOneLine - 1) + restWidth));
+    constElementsPerOneLine = elementsPerOneLine;
+    rest = quantity % elementsInOneBox;
     translateBrands();
 });
 
-$(".prev").on('click', function () {
-    
-    if(left!=quantity-elementsInOneBox)
-    {
-    if(elementsInOneBox == 1){
-        tutej -= brandBoxWidth;
-        left += elementsInOneBox;
-    }
-    else if(left<=elementsInOneBox)
-    {
-        tutej -= brandBoxWidth*elementsInOneBox;
-        left += elementsInOneBox;
-    }
-    else{
-        left += quantity%elementsInOneBox;
-        tutej = 0;
-    }
-    $(".brandsCaption").text(left+"/"+ left%elementsInOneBox);
+function getElementsInTheBox() {
+    let contentWidth = $(".content").width();
 
-    translateBrands();
+    if (contentWidth >= 1150) {
+        return 5;
+    } else if (contentWidth >= 950) {
+        return 4;
+    } else if (contentWidth >= 700) {
+        return 3;
+    } else {
+        return 2;
+    }
 }
-});
 
-function translateBrands(){
-    $(".brands").css("transform", "translateX(" + (-tutej) + "px)");
-    $(".prev").css("transform", "translateX(" + tutej + "px)");
-    $(".next").css("transform", "translateX(" + tutej + "px)");
+function getBrandBoxWidth() {
+    return $(".brand").width();
 }
-*/
+
 function translateBrands() {
-    $(".brands").css("transform", "translateX(" + (-pos) + "px)");
-    $(".prev").css("transform", "translateX(" + pos + "px)");
-    $(".next").css("transform", "translateX(" + pos + "px)");
+    $(".brands").css("transform", "translateX(" + (-position) + "px)");
+    $(".prev").css("transform", "translateX(" + position + "px)");
+    $(".next").css("transform", "translateX(" + position + "px)");
 }
